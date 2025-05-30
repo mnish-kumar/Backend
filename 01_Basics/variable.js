10dde0588c7d97d49e27a5549dccec4422e22b7e
Manish Kumar <154582626+mnish-kumar@users.noreply.github.com>
2025-05-30 16:48:24 +0000

    Know all about Variable

---
+++ 01_Basics/variable.js
@@ -0,0 +1,23 @@
+// 1.const = which is one time define not change again
+const accountId = 12345
+
+//2. let = this is help to avoid scope{}
+let accountEmail = "manishkr@s.gmail.com"
+
+//3. var = Try to avoid used this variable
+var accountPassword = "mnishkr"
+
+
+
+// in javascript wi
+var accountSate;
+
+
+// we define const one try to change again const variable -> ERROR
+// accountId = 9006
+
+
+
+
+// try to print multiple variable use console.table
+console.table([accountId , accountEmail , accountPassword , accountSate])
