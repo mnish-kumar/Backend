910d03662ccd66234918c251bcea9984430ef522
Manish Kumar <154582626+mnish-kumar@users.noreply.github.com>
2025-05-31 17:15:34 +0000

    Operation in String

---
+++ 01_Basics/operations.js
@@ -0,0 +1,38 @@
+let value = 3;
+let negValue = -value;
+console.log(negValue);
+
+
+//Addition
+console.log(2 + 2);
+
+//Substraction
+console.log(5 - 3);
+
+//Multiply
+console.log(2 * 3);
+
+//Pow
+console.log(2 ** 3);
+
+//find remainder
+console.log(4 % 2);
+
+
+//Append two string
+let str1 = "Manish";
+let str2 = " kumar";
+console.log(str1 + str2);
+
+
+//Problem here :
+console.log("1" + 2);    //op -> 12
+console.log(1 + "2");    //op -> 12
+
+
+let a = 2;
+console.log(++a);
+
+
+
+
