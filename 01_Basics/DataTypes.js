4f2d3034f4e02d28a90941837f50ebbde3fddaff
Manish Kumar <154582626+mnish-kumar@users.noreply.github.com>
2025-05-31 15:23:05 +0000

    Learn Data types

---
+++ 01_Basics/DataTypes.js
@@ -0,0 +1,21 @@
+console.log("All about the dataTypes : 👍");
+
+/* 
+    1.number -> 2 to the power 53
+    2.bigint -> used in store long number
+    3.string -> ""
+    4.boolean -> true , false;
+    5.null (stand alone value) -> Representation of empty
+    6.undefined -> not asign value
+    7.symbol -> use for uniqueness
+
+*/
+
+
+// Know the what types of input used this
+console.log(typeof "Manish Kumar");
+
+let age = 9;
+console.log(typeof age);
+
+console.log(typeof null);
