0cefc28bb9180473d263732e2f618ead571cb6d7
Manish Kumar <154582626+mnish-kumar@users.noreply.github.com>
2025-06-02 15:29:53 +0000

    Data type summary

---
+++ 01_Basics/02_Data-Types.js
@@ -0,0 +1,34 @@
+console.log("Hello");
+
+// Primitive Data-types
+// String , Number , Boolean , null , undefined , symbol
+
+// Reference type (Non primitive)
+// Array , Object , Functions
+
+
+const score = 100;
+const scoreValue = 100.3;
+
+const id = Symbol('123');
+const AnotherId = Symbol('123');
+console.log(id == AnotherId);
+
+
+// Array
+const Array = ["batman" , "kumfo" , "range"];
+console.log(Array);
+
+
+// Objects
+const myObj = {
+    name : "Manish Kumar",
+    age : 21,
+}
+console.log(myObj);
+
+
+// functions
+const myFunction = function() {
+    console.log("Hello Bhai Badhiya.");
+}
